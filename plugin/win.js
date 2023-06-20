e23acb23086e4226d7714f1569fbd4840fd79b50
itldg <wanglong126@139.com>
2023-06-20 21:55:08 +0800

    feat: 增加自定义参数设置

--- plugin/win.js
+++ plugin/win.js
@@ -1,24 +1,67 @@
-window.init = async () => {
-	//基本选项
-	const options = {
-		//字宽
-		fontSize: 20,
-		//最小字体尺寸
-		minFontSize: 6,
-		//边框尺寸
-		border: 10,
-		//最大宽度
-		maxWidth: 600,
-	}
-	let fontSize = localStorage.getItem('fontSize') ?? options.fontSize
-	changeFontSize(fontSize)
+const userOption = {
+	//明亮颜色
+	lightColor: '#f8f7b6',
+	//明亮字体颜色
+	lightFontColor: '#000000',
+	//暗黑颜色
+	darkColor: '#303133',
+	//暗黑字体颜色
+	darkFontColor: '#999999',
+	//透明度
+	opacity: 9,
+	//字体大小
+	fontSize: 20,
+
+	//最小字体尺寸
+	minFontSize: 6,
+	//边框尺寸
+	border: 10,
+	//最大宽度
+	maxWidth: 600,
+}
+let options = {}
+
+function show(options) {
+	const elstyle = document.documentElement.style
+	elstyle.setProperty('--font-size', options.fontSize + 'px')
+	elstyle.setProperty('--light-font-color', options.lightFontColor)
+	elstyle.setProperty('--light-color', hexToRgba(options.lightColor, options.opacity / 10))
+	elstyle.setProperty('--dark-font-color', options.darkFontColor)
+	elstyle.setProperty('--dark-color', hexToRgba(options.darkColor, options.opacity / 10))
+
 	//根据系统主题更改主题
 	const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-	isDark && document.body.classList.add('dark')
-
+	isDark ? document.body.classList.add('dark') : document.body.classList.remove('dark')
+}
+window.init = async () => {
+	let lastOption = localStorage.getItem('option')
+	options = {
+		...userOption,
+		...JSON.parse(lastOption),
+	}
+	show(options)
 	//dom元素
 	const content = document.getElementById('content')
-
+	const option = document.getElementById('option')
+	document.getElementById('setting-btn').addEventListener('click', () => {
+		if (window.outerWidth < 200 || window.outerHeight < 400) {
+			window.resize(200, 400)
+		}
+		option.style.display = 'block'
+		document.body.classList.add('open')
+		setOption()
+	})
+	document.getElementById('option-cancel').addEventListener('click', () => {
+		document.body.classList.remove('open')
+		option.style.display = 'none'
+		show(options)
+	})
+	document.getElementById('option-save').addEventListener('click', () => {
+		document.body.classList.remove('open')
+		option.style.display = 'none'
+		options = { ...optionTemp }
+		saveOptions()
+	})
 	//读取历史数据
 	const lastText = localStorage.getItem('lastText') ?? ''
 	content.value = lastText
@@ -35,7 +78,7 @@ window.init = async () => {
 		}
 		//末尾+10 适应滚动条
 		const width = (maxWidth > options.maxWidth ? options.maxWidth : maxWidth) + options.border * 2 + 10
-		window.resize(width, fontSize)
+		window.resize(width, options.fontSize)
 		autoHeight(width)
 	} else {
 		console.log('使用上次缓存内容')
@@ -63,7 +106,7 @@ window.init = async () => {
 		if (event.key === 'Escape') {
 			if (escFirst) {
 				clearTimeout(closeTimer)
-				localStorage.clear()
+				localStorage.removeItem('lastText')
 				window.close()
 				return
 			}
@@ -85,13 +128,13 @@ window.init = async () => {
 			// 判断滚轮方向
 			if (event.deltaY < 0) {
 				// 放大字体
-				fontSize++
+				options.fontSize++
 			} else {
 				// 缩小字体
-				fontSize--
-				fontSize < options.minFontSize && (fontSize = options.minFontSize)
+				options.fontSize--
+				options.fontSize < options.minFontSize && (options.fontSize = options.minFontSize)
 			}
-			changeFontSize(fontSize)
+			changeFontSize()
 		}
 	})
 
@@ -106,7 +149,7 @@ window.init = async () => {
 	}
 	//绑定拖拽移动事件
 	document.addEventListener('mousedown', (event) => {
-		if (event.button === 0 && event.target.tagName !== 'TEXTAREA') {
+		if (event.button === 0 && event.target.id === 'container') {
 			moveIng = true
 			startX = parseInt(event.clientX)
 			startY = parseInt(event.clientY)
@@ -128,6 +171,27 @@ window.init = async () => {
 		const height = content.scrollHeight + options.border * 2
 		// console.log('高度', height)
 		window.resize(width, height)
+	}
+	let optionTemp = {}
+	//绑定参数更新事件
+	document.getElementById('option').addEventListener('input', (event) => {
+		const target = event.target
+		const name = target.id
+		if (optionTemp[name] === undefined) return
+		const value = target.value
+		optionTemp[name] = value
+		show(optionTemp)
+	})
+
+	//更新设置界面
+	function setOption() {
+		for (const key in options) {
+			const el = document.getElementById(key)
+			if (el) {
+				el.value = options[key]
+			}
+		}
+		optionTemp = { ...options }
 	}
 }
 
@@ -154,10 +218,27 @@ function getTextWidth(text, font) {
 }
 
 /**
- * 修改页面字体大小(修改CSS变量的方式)
- * @param {Number} newFontSize 新的字体大小
+ * 颜色格式 hex 转 rgba
+ * @param {*} bgColor
+ * @param {*} opacity
+ * @return {*}
  */
-function changeFontSize(newFontSize) {
-	document.documentElement.style.setProperty('--font-size', newFontSize + 'px')
-	localStorage.setItem('fontSize', newFontSize)
+function hexToRgba(bgColor, opacity = 1) {
+	let color = bgColor.slice(1) // 去掉'#'号
+	let rgba = [parseInt('0x' + color.slice(0, 2)), parseInt('0x' + color.slice(2, 4)), parseInt('0x' + color.slice(4, 6)), opacity]
+	return 'rgba(' + rgba.toString() + ')'
+}
+
+/**
+ * 修改页面字体大小(修改CSS变量的方式)
+ */
+function changeFontSize() {
+	document.documentElement.style.setProperty('--font-size', options.fontSize + 'px')
+	saveOptions()
+}
+/**
+ * 保存设置
+ */
+function saveOptions() {
+	localStorage.setItem('option', JSON.stringify(options))
 }
